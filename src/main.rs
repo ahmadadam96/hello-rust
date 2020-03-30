1583a8055da2e863be747612a8827a076a779393
Ahmed El Yaacoub <aaey@kth.se>
2020-03-30 16:18:47 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+    println!("I'm a Rustacean!");
+    //println!("Addition is {}", add(10,50));
+}
+
+/* fn add(number_1: u64, number_2: u64) -> u64 {
+    let mut number_3 = number_1 + number_2;
+
+    if number_3 >= 43 {
+        number_3 += 3;
+    }
+
+    return number_3;
+}
+ */
